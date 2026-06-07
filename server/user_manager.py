558eec9606a4db67ba6db363ddf088e75f1ae3dd
Satinder singh <satindersingh@Satinders-MacBook-Air.local>
2026-06-08 00:17:21 +0200

    add get_username and username_exists methods to UserManager

--- server/user_manager.py
+++ server/user_manager.py
@@ -1,16 +1,21 @@
+from multiprocessing import connection
+
+
 class UserManager:
     def __init__(self):
         self.users = {}  # Dictionary to store user information
-
+        self.sockets = {}  # Dictionary to store user connections
     def add_user(self, username, connection):
         if username in self.users:
             raise ValueError(f"username '{username}' already exists.")
         else:
             self.users[username] = connection
+            self.sockets[connection] = username
         
     def remove_user(self, username):
         if username in self.users:
             del self.users[username]
+            del self.sockets[connection]
         else:
             raise ValueError(f"username '{username}' does not exist.")
         
@@ -19,6 +24,18 @@ class UserManager:
             return self.users[username]
         else:
             raise ValueError(f"username '{username}' does not exist.")
-    
+        
+    def get_username(self, connection):
+        if connection in self.sockets:
+            return self.sockets[connection]
+        else:
+            raise ValueError(f"connection '{connection}' does not exist.")
+
+    def username_exists(self, username):
+        if username in self.users:
+            return username in self.users
+        else:
+            raise ValueError(f"username '{username}' does not exist.")
+        
     def get_all_users(self):
         return list(self.users.keys())
