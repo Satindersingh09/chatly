759a12703ed00f949bf79c2636859106b4a5fcb2
Satinder singh <satindersingh@Satinders-MacBook-Air.local>
2026-06-09 19:28:58 +0200

    adding broadcast support and changing del logic to only del if empty

--- server/room_manager.py
+++ server/room_manager.py
@@ -5,26 +5,53 @@ class RoomManager:
     def create_room(self, room_name):
         if room_name in self.rooms:
             raise ValueError(f"room '{room_name}' already exists.")
-        self.rooms[room_name] = []  # initialize an empty list to store users in the room
+        else:
+            self.rooms[room_name] = []  # initialize an empty list to store users in the room
 
-    def join_room(self, room_name, username):
+    def join_room(self, room_name, socket, username):
         if room_name not in self.rooms:
             raise ValueError(f"room '{room_name}' does not exist.")
-        if username in self.rooms[room_name]:
-            raise ValueError(f"username '{username}' already in room '{room_name}'.")
-        self.rooms[room_name].append(username)  # add user to the room
+        if socket in self.rooms[room_name]:
+            raise ValueError(f"socket '{username}' already in room '{room_name}'.")
+        self.rooms[room_name].append(socket)  # add user to the room
 
-    def leave_room(self, room_name, username):
+    def leave_room(self, room_name, socket, username):
         if room_name not in self.rooms:
             raise ValueError(f"room '{room_name}' does not exist.")
-        if username not in self.rooms[room_name]:
-            raise ValueError(f"username '{username}' not in room '{room_name}'.")
-        self.rooms[room_name].remove(username)  # remove user from the room
+        if socket not in self.rooms[room_name]:
+            raise ValueError(f"socket '{username}' not in room '{room_name}'.")
+        self.rooms[room_name].remove(socket)  # remove user from the room
 
-    def track_user_in_room(self, room_name):
+    def remove_user_from_all_rooms(self, socket, username):
+        for room_name, sockets in self.rooms.items():
+            if socket in sockets:
+                sockets.remove(socket)  # remove user from all rooms they are in
+
+    def delete_room(self, room_name):
+        if room_name not in self.rooms:
+            raise ValueError(f"room '{room_name}' does not exist.")
+        if len(self.rooms[room_name]) == 0:
+            del self.rooms[room_name]  # delete the room if its empty
+
+    def track_members(self, room_name):
         if room_name not in self.rooms:
             raise ValueError(f"room '{room_name}' does not exist.")
         return list(self.rooms[room_name])  # return a copy of the list of users in the room
+    
+    def get_user_rooms(self, socket, username):
+        user_rooms = []
+        for room_name, sockets in self.rooms.items():
+            if socket in sockets:
+                user_rooms.append(room_name)
+                print(f"{username} is in room #{room_name}")  # print which room user is in
+        return user_rooms
+    
+    def broadcast_message(self, room_name, message, sender_socket):
+        if room_name not in self.rooms:
+            raise ValueError(f"room '{room_name}' does not exist.")
+        for socket in self.rooms[room_name]:
+            if socket != sender_socket:  # send message to all users except the sender
+                socket.send(message.encode('utf-8'))  # send message to the user
 
     def get_all_rooms(self):
         return list(self.rooms.keys())  # return a list of all room names
